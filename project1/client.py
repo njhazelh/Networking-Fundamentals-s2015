afc86a7e82eab868dad030179a8b12f17a411c3c
Nicholas Jones <njhazelh@gmail.com>
2015-01-26 13:16:43 -0500

    This should work
    Needs more documentation, testing, and stability in error cases.

--- project1/client.py
+++ project1/client.py
@@ -1,18 +1,53 @@
 #!/usr/bin/python
 
-import sys, re, argparse
+import re
+import argparse
+import socket
+import ssl
+
+import messages
+
 
 __author__ = 'Nick Jones'
 
-parser = argparse.ArgumentParser(description="Do lots of meaningless math over the network!")
-parser.add_argument("-p", "--port", help="The server port to connect to", type=int, default=27993)
-parser.add_argument("-s", "--secure", help="Use a secure socket", action='store_true')
-parser.add_argument("hostname", help="The hostname of the server")
-parser.add_argument("id", help="The NEU id of the student running the program.  Must have all leading zeros.")
-args = parser.parse_args()
-print(args)
 
-def main():
+def model_data(data):
+    statusRegex = re.compile("cs5700spring2015 STATUS (\d+) ([\+,\-,\*,\/]) (\d+)\n")
+    result = statusRegex.match(data)
+    if result is not None:
+        return messages.StatusMessage(int(result.group(1)), result.group(2), int(result.group(3)))
+    else:
+        byeRegex = re.compile("cs5700spring2015 (.*) BYE\n")  # TODO: figure out regex for 64 bytes.  [A-F,0-9]{64}
+        return messages.ByeMessage(byeRegex.match(data).group(1))
+
+
+def parse_message(conn, data):
+    """
+    This function will find which type of message the data is and create a response.
+    :param data: The data from the server
+    :return:
+    """
+    message = model_data(data)
+    message.do(conn)
+    return message.isFinal()
+
+
+def make_connection(secure, hostname, port):
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if secure:
+        conn = ssl.wrap_socket(conn)
+    conn.connect((hostname, port))
+    return conn
+
+
+def send_recv_loop(conn):
+    while True:
+        final = parse_message(conn, conn.read(256))
+        if final:
+            break
+
+
+def main(args):
     """
     This is the entry point for the program.
 
@@ -23,12 +58,16 @@ def main():
 
     :return: Return nothing.
     """
-    # TODO: Create Socket Connection
-    # TODO: Send initial message
-    # TODO: DO ALL THE MESSAGE PASSING!
-    # TODO: Catch the final value and exit
-    pass
+    conn = make_connection(args.secure, args.hostname, args.port)
+    conn.write("cs5700spring2015 HELLO {}\n" % args.id)
+    send_recv_loop(conn)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Do lots of meaningless math over the network!")
+    parser.add_argument("-p", "--port", help="The server port to connect to", type=int, default=27993)
+    parser.add_argument("-s", "--secure", help="Use a secure socket", action='store_true')
+    parser.add_argument("hostname", help="The hostname of the server")
+    parser.add_argument("id", help="The NEU id of the student running the program.  Must have all leading zeros.")
+    args = parser.parse_args()
+    main(args)
---
+++ project1/messages.py
@@ -0,0 +1,67 @@
+__author__ = 'Nick'
+
+
+class Message(object):
+    """
+    This class serves as an interface for all other Messsage subclasses.
+    """
+
+    def do(self, connection):
+        raise NotImplementedError("do was not implemented")
+
+    def is_final(self):
+        raise NotImplementedError("isFinal was not implemented")
+
+
+class StatusMessage(Message):
+    """
+    This class represents the data contained in a Status message from the server.
+    Such a message takes the form:
+        cs5700spring2015 STATUS [0-1000] {+,-,*,/] [0-1000]\n
+    The correct response to this message is the answer to the operation.
+        cs5700spring2015 [solution]\n
+    """
+
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+    @property
+    def answer(self):
+        if self.op == "+":
+            return self.left + self.right
+        elif self.op == "-":
+            return self.left - self.right
+        elif self.op == "*":
+            return self.left * self.right
+        elif self.op == "/":
+            return self.left / self.right
+        else:
+            raise Exception("Operation not recognized")
+
+    def is_final(self):
+        return False
+
+    def do(self, connection):
+        connection.write("cs2500spring2015 {}\n".format(self.answer))
+
+
+class ByeMessage(Message):
+    """
+    This class represents the data contained in the Bye message from the server.
+    Such a message takes the form:
+        cs5700spring2015 [64 byte secret] BYE\n
+    The correct response to this is to print the secret and close the connection.
+    """
+
+    def __init__(self, secret):
+        self.secret = secret
+
+    def is_final(self):
+        return True
+
+    def do(self, connection):
+        print(self.secret)
+        connection.shutdown()
+        connection.close()
