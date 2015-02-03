f92afe7e054593335cd0beb569ba6e6c11615abc
Nicholas Jones <njhazelh@gmail.com>
2015-02-02 19:34:01 -0500

    Added documentation and exception on malformed message.
    Still need to add unit tests.

---
+++ project1/README.md
@@ -0,0 +1,42 @@
+#Team the-one-percent
+=====================
+This project was designed according to the standards laid out in 
+[Project 1](http://david.choffnes.com/classes/cs4700sp15/project1.php)
+
+##Approach
+==========
+The idea behind this project was rather simple.  There was little need to
+maintain state, and no need to utilize lower level feature such as memory 
+management.  What was important was a simple interface with network sockets, the 
+ability to easily add SSL/TLS encryption, and strong String parsing ability. 
+For this reason, we chose Python as our implementation language.
+
+Our first step was to handle the argument parsing from the command line.  At
+first, we attempted to do this using regular expressions, but Python's built-in
+arg-parse library turned out to be much simpler.
+
+Next, we implemented a quick method to acquire a configured socket connection.
+Python's socket library made this simple, and adding ssl was not much more
+difficult.
+
+From here, we only had to handle the message parsing and responses.  To do this,
+we wanted to abstract from Strings as quickly as possible.  Therefore, we
+implemented a Message class that used regular expressions to match and extract
+message parameters into one of several Message sub-classes. Each of these
+sub-classes had the same interface, so after converting, all we had to worry
+about was calling the do method to react and the isFinal method to check if we
+should exit.
+
+##Challenges
+============
+The most significant challenge encountered was learning Python. Although we had
+some experience working with the language, it took a little work to work out
+the structure that used Python in the most appropriate manner.
+
+Additionally, we needed to build the correct regex patterns to match the
+messages received.
+
+##Testing
+=========
+Initially we tested our code by running it as it progressed. After achieving a
+functional state, we stabilized the code and added unit tests #TDD.
--- project1/messages.py
+++ project1/messages.py
@@ -1,17 +1,51 @@
-import math, socket
+import math
+import socket
+import re
 
 __author__ = 'Nick'
 
 
 class Message(object):
     """
-    This class serves as an interface for all other Messsage subclasses.
+    This class serves as an interface for all other Message subclasses.
     """
 
+    @staticmethod
+    def modelData(data):
+        """
+        Take a message in a String and convert it to the appropriate object.
+        :param data: The message from the server
+        :return: a sub-class of Message representing the data.
+        """
+        types = [StatusMessage, ByeMessage]
+
+        for type in types:
+            result = type.match(data)
+            if result is not None:
+                return type.fromRegex(result)
+
+        raise MessageParseException("Message not recognized: {}".format(data))
+
+    @classmethod
+    def fromRegex(cls, regexMatch):
+        """
+        This method provides a way to construct an instance of this class from
+        the result of a regex match.
+        """
+        raise NotImplementedError("fromRegex was not implemented")
+
     def do(self, connection):
+        """
+        React to the message in the appropriate manner.
+        :param connection: The socket to reply on.
+        """
         raise NotImplementedError("do was not implemented")
 
     def is_final(self):
+        """
+        Is this the final message from the server?
+        :return: True if this is the final message from the server
+        """
         raise NotImplementedError("isFinal was not implemented")
 
 
@@ -25,12 +59,41 @@ class StatusMessage(Message):
     """
 
     def __init__(self, left, op, right):
+        """
+        Constructor using data from an expression. eg. 3 + 45
+        :param left: The number on the left of the expression. eg. 3.
+        :param op: The operation from the expression. eg. +.
+        :param right: The right number from the expression. eg. 45.
+        """
         self.left = left
         self.op = op
         self.right = right
 
+    @classmethod
+    def fromRegex(cls, regexMatch):
+        """
+        Construct an instance of StatusMessage from the result of a match.
+        :param regexMatch: The result of a regex match operation.  See match below.
+        :return: A StatusMessage object.
+        """
+        return cls(int(regexMatch.group(1)), regexMatch.group(2), int(regexMatch.group(3)))
+
+    @staticmethod
+    def match(data):
+        """
+        Match a String to this message type.
+        :param data: The data to match against this message.
+        :return: None if the message does not match, else a matched object with the
+        significant data in groups.
+        """
+        pattern = re.compile("cs5700spring2015 STATUS (\d+) ([\+,\-,\*,/]) (\d+)\n")
+        return pattern.match(data)
+
     @property
     def answer(self):
+        """
+        Get the result of expression
+        """
         if self.op == "+":
             return self.left + self.right
         elif self.op == "-":
@@ -43,9 +106,17 @@ class StatusMessage(Message):
             raise Exception("Operation not recognized")
 
     def is_final(self):
+        """
+        This is not a final message
+        :return: False
+        """
         return False
 
     def do(self, connection):
+        """
+        Respond to the message from the server with the solution.
+        :param connection: The socket to communicate over.
+        """
         response = "cs5700spring2015 {}\n".format(self.answer)
         connection.sendall(response.encode())
 
@@ -59,12 +130,62 @@ class ByeMessage(Message):
     """
 
     def __init__(self, secret):
+        """
+        :param secret: The 64-byte secret form the message.
+        :return: A ByeMessage representing a message from the server.
+        """
         self.secret = secret
 
+    @classmethod
+    def fromRegex(cls, regexMatch):
+        """
+        Create an instance of ByeMessage using the data contained in a regex
+        match.  See match below.
+        :param regexMatch: The match information from the match below.
+        :return: An instance of ByeMessage
+        """
+        return cls(regexMatch.group(1))
+
+    @staticmethod
+    def match(data):
+        """
+        Match a String to this message type and extract the important information.
+        :param data: The String to match against.
+        :return: A regex match object if the String matches.  Else, None.
+        """
+        pattern = re.compile("cs5700spring2015 ([a-fA-F0-9]+) BYE\n")
+        return pattern.match(data)
+
     def is_final(self):
+        """
+        This is the final message from the server.
+        :return: True
+        """
         return True
 
     def do(self, connection):
+        """
+        Print out the secret and shutdown the socket connection.
+        :param connection: The connection to close.
+        """
         print(self.secret)
         connection.shutdown(socket.SHUT_RDWR)
         connection.close()
+
+
+class MessageParseException(Exception):
+    """
+    This is an Exception for when the message from the server does
+    not match any of the correct formats.
+    """
+    pass
+
+
+class EmptyMessageException(Exception):
+    """
+    This is an Exception for when the message from the server doesn't
+    contain anything.  This usually happens when we said something
+    wrong.  For example, we answered a STATUS with the incorrect
+    solution, or we used an incorrect format.
+    """
+    pass
