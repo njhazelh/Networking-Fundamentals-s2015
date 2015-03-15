713b2be6d60764cd4aafd1497178154e81fdbfb9
Michelle Suk <msuk@users.noreply.github.com>
2015-03-15 03:26:43 -0400

    Changed format of experiment1 file to match Nick's

--- project3/experiment1.tcl
+++ project3/experiment1.tcl
@@ -1,34 +1,78 @@
-
 # Experiment 1
 
-proc exp1_test {} {
+set name "experiment1"
+set iterations 1000
+
+# TCP Variants
+set RENO "Reno"
+set NEW_RENO "NewReno"
+set VEGAS "Vegas"
+set VARIANT_COMBOS {{RENO RENO}
+                    {NEW_RENO RENO}
+                    {VEGAS VEGAS}
+                    {NEW_RENO VEGAS}}
+
+
+# Open files for output
+set f [open "results/$name/out.tr" w]
+$ns trace-all $f
+set nf [open "results/$name/out.nam" w]
+$ns namtrace-all $nf
+
+proc record-result {} {
+
+}
+
+
+proc run-single-test {} {
 	
 	# Create a simulator object
 	set ns [new Simulator]
 
 	# Create six nodes
-	set n0 [$ns node]
 	set n1 [$ns node]
 	set n2 [$ns node]
 	set n3 [$ns node]
 	set n4 [$ns node]
 	set n5 [$ns node]
+	set n6 [$ns node]
 
 	# Create links between nodes
-	set duplex-link $n0 $n1 10Mb
-	set duplex-link $n1 $n4 10Mb
-	set duplex-link $n1 $n2 10Mb
-	set duplex-link $n2 $n3 10Mb
-	set duplex-link $n2 $n5 10Mb
+	set duplex-link $n1 $n2 10Mb 10ms DropTail
+	set duplex-link $n2 $n5 10Mb 10ms DropTail
+	set duplex-link $n2 $n3 10Mb 10ms DropTail
+	set duplex-link $n3 $n4 10Mb 10ms DropTail
+	set duplex-link $n3 $n6 10Mb 10ms DropTail
 
-	# TO DO
-	#	Determine which TCP variant is being tested
-	#	Open trace file
-	# 	Set sinks
-	#	CBR flow rate
+	$ns duplex-link-op $n1 $n2 orient right-down
+    $ns duplex-link-op $n2 $n5 orient right-up
+    $ns duplex-link-op $n2 $n3 orient right
+    $ns duplex-link-op $n3 $n4 orient right-up
+    $ns duplex-link-op $n3 $n6 orient right-down
 
-	# Finish procedure
-	proc finish {} {
-		
-	}
 }
+
+proc run-experiment-1 {} {
+    foreach {combo} $VARIANT_COMBOS {
+        set tcp1 [lindex $combo 0]
+        set tcp2 [lindex $combo 1]
+        # generate random config
+
+        for {set i 0} {$i<iterations} {incr x} {
+            run-single-test $i $tcp1 $tcp2 #other config
+        }
+    }
+}
+
+proc finish {} {
+    global ns f nf
+    $ns flush-trace
+    close $f
+    close $nf
+
+    puts "running nam..."
+    exec nam out.nam &
+    exit 0
+}
+
+run-experiment-1
