42d88799702bf41130a1f560e7d7f45b72f7d11b
njhazelh <njhazelh@gmail.com>
2015-03-15 22:57:13 -0400

    Getting output from Experiment 2.

--- project3/experiment2.tcl
+++ project3/experiment2.tcl
@@ -10,124 +10,160 @@
 # Vegas/Vegas
 # NewReno/Vegas
 
-set name "experiment2" # used to name folders
-set iterations 1000
+set NAME "experiment2"
+set ITERATIONS 10
 
 # TCP Variants
-set RENO "Reno"
-set NEW_RENO "NewReno"
-set VEGAS "Vegas"
-set VARIANT_COMBOS {{RENO RENO}
-                    {NEW_RENO RENO}
-                    {VEGAS VEGAS}
-                    {NEW_RENO VEGAS}}
+set RENO 0
+set NEW_RENO 1
+set VEGAS 2
+set UDP 3
+set VARIANT_COMBOS {{0 0} {1 0} {2 2} {1 2}}
+
+set ns [new Simulator]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
 
 # Open files for output
-set f [open "results/$name/out.tr" w]
-$ns trace-all $f
-set nf [open "results/$name/out.nam" w]
-$ns namtrace-all $nf
-
-set udp0 [new Agent/UDP]
-$ns attach-agent $n2 $udp0
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 attach-agent $udp0
-$udp0 set class_ 0
-
-set null0 [new Agent/Null]
-$ns attach-agent $n3 $null0
-
-$ns connect $udp0 $null0
-$ns at 0.5 "$cbr0 start"
-
-$cbr0 set packetSize_ 500
-$cbr0 set interval_ 0.005
-
-set tcp [new Agent/TCP]
-$tcp set class_ 1
-$ns attach-agent $n1 $tcp
-
-set sink [new Agent/TCPSink]
-$ns attach-agent $n4 $sink
-
-$ns connect $tcp $sink
-
-$ns color 0 Blue
-$ns color 1 Red
-
-$ns at 3.0 "finish"
+#puts $file "TCP1, TCP2, Time, CBR Bandwidth, TCP1 Bandwidth, TCP2 Bandwidth, TCP1 Loss Rate, TCP2 Loss Rate, TCP1 Latency, TCP2 Latency"
 
 # copied from http://www.isi.edu/nsnam/ns/tutorial/nsscript4.html#second
-proc record-result {} {
-    global sink0 sink1 sink2 f0 f1 f2
-    #Get an instance of the simulator
-    set ns [Simulator instance]
-    #Set the time after which the procedure should be called again
-    set time 0.5
-    #How many bytes have been received by the traffic sinks?
-    set bw0 [$sink0 set bytes_]
-    set bw1 [$sink1 set bytes_]
-    set bw2 [$sink2 set bytes_]
+proc finish-and-record {ns cbr_bw tcp1_id tcp2_id sink1 sink2} {
+    global file
+
     #Get the current time
     set now [$ns now]
+
+    #How many bytes have been received by the traffic sinks?
+    set tcp1_name [enum->tcp_name $tcp1_id]
+    set tcp1_bytes [$sink1 set bytes_]
+    set tcp1_bw [expr $tcp1_bytes/$now*8/1000000]
+    set tcp1_loss 0
+    set tcp1_lat 0
+
+    set tcp2_name [enum->tcp_name $tcp2_id]
+    set tcp2_bytes [$sink2 set bytes_]
+    set tcp2_bw [expr $tcp2_bytes/$now*8/1000000]
+    set tcp2_loss 0
+    set tcp2_lat 0
+
+    set line "$tcp1_name, $tcp2_name, $now, $cbr_bw, $tcp1_bw, $tcp2_bw, $tcp1_loss, $tcp2_loss, $tcp1_lat, $tcp2_lat"
+
     #Calculate the bandwidth (in MBit/s) and write it to the files
-    puts $f0 "$now [expr $bw0/$time*8/1000000]"
-    puts $f1 "$now [expr $bw1/$time*8/1000000]"
-    puts $f2 "$now [expr $bw2/$time*8/1000000]"
-    #Reset the bytes_ values on the traffic sinks
-    $sink0 set bytes_ 0
-    $sink1 set bytes_ 0
-    $sink2 set bytes_ 0
-    #Re-schedule the procedure
-    $ns at [expr $now+$time] "record"
+    puts $line
+
+    $ns halt
+    delete $ns
+    exit 0
 }
 
-proc run-single-test {number tcp1 tcp2} {
-    reset
-    set ns [new Simulator]
-    set n1 [$ns node]
-    set n2 [$ns node]
-    set n3 [$ns node]
-    set n4 [$ns node]
-    set n5 [$ns node]
-    set n6 [$ns node]
+proc enum->tcp {enum} {
+    global RENO NEW_RENO VEGAS
+    if {$enum == $RENO} {
+        return [new Agent/TCP/Reno]
+    } elseif {$enum == $NEW_RENO} {
+        return [new Agent/TCP/Newreno]
+    } elseif {$enum == $VEGAS} {
+        return [new Agent/TCP/Vegas]
+    } else {
+        error "enum->tcl_name: $enum is not  a TCP id"
+    }
+}
 
-    $ns duplex-link $n1 $n2 10Mb 10ms DropTail
-    $ns duplex-link $n5 $n2 10Mb 10ms DropTail
-    $ns duplex-link $n2 $n3 10Mb 10ms DropTail
-    $ns duplex-link $n3 $n4 10Mb 10ms DropTail
-    $ns duplex-link $n3 $n6 10Mb 10ms DropTail
+proc enum->tcp_name {enum} {
+    global RENO NEW_RENO VEGAS
+    if {$enum == $RENO} {
+        return "Reno"
+    } elseif {$enum == $NEW_RENO} {
+        return "NewReno"
+    } elseif {$enum == $VEGAS} {
+        return "Vegas"
+    } else {
+        error "enum->tcl_name: $enum is not  a TCP id"
+    }
+}
 
-    $ns duplex-link-op $n1 $n2 orient right-down
-    $ns duplex-link-op $n5 $n2 orient right-up
-    $ns duplex-link-op $n2 $n3 orient right
-    $ns duplex-link-op $n3 $n4 orient right-up
-    $ns duplex-link-op $n3 $n6 orient right-down
+proc run-single-test {tcp1_id tcp2_id cbr_bw} {
+    global n1 n2 n3 n4 n5 n6
+
+    set ns [Simulator instance]
+
+    set tcp1_start [expr {5.0 * rand()}]
+    set tcp2_start [expr {5.0 * rand()}]
+
+    # configure links
+    $ns duplex-link $n1 $n2 10Mb 2ms DropTail
+    $ns duplex-link $n5 $n2 10Mb 2ms DropTail
+    $ns duplex-link $n2 $n3 10Mb 2ms DropTail
+    $ns duplex-link $n3 $n4 10Mb 2ms DropTail
+    $ns duplex-link $n3 $n6 10Mb 2ms DropTail
+
+    # Add CBR Flow
+    set udp0 [new Agent/UDP]
+    set null0 [new Agent/Null]
+    set cbr0 [new Application/Traffic/CBR]
+    $udp0 set class_ 0
+    $ns attach-agent $n2 $udp0
+    $ns attach-agent $n3 $null0
+    $ns connect $udp0 $null0
+    # Configure CBR Bandwidth
+    $cbr0 attach-agent $udp0
+    $cbr0 set packetSize_ 500
+    $cbr0 set interval_ 0.005
+
+    # Add TCP1 Flow
+    set tcp1_inst [enum->tcp $tcp1_id]
+    set sink1 [new Agent/TCPSink]
+    set cbr1 [new Application/Traffic/CBR]
+    $tcp1_inst set class_ 1
+    $ns attach-agent $n1 $tcp1_inst
+    $ns attach-agent $n4 $sink1
+    $ns connect $tcp1_inst $sink1
+    # Add Traffic to TCP1 Flow
+    $cbr1 attach-agent $tcp1_inst
+    $cbr1 set packetSize_ 500
+    $cbr1 set interval_ 0.005
+
+    # Add TCP2 Flow
+    set tcp2_inst [enum->tcp $tcp2_id]
+    set sink2 [new Agent/TCPSink]
+    set cbr2 [new Application/Traffic/CBR]
+    $tcp2_inst set class_ 2
+    $ns attach-agent $n5 $tcp2_inst
+    $ns attach-agent $n6 $sink2
+    $ns connect $tcp2_inst $sink2
+    # Add Traffic to TCP2 Flow
+    $cbr2 attach-agent $tcp2_inst
+    $cbr2 set packetSize_ 500
+    $cbr2 set interval_ 0.005
+
+    $ns at 0 "$cbr0 start"
+    $ns at $tcp1_start "$cbr1 start"
+    $ns at $tcp2_start "$cbr2 start"
+    $ns at 60 "finish-and-record $ns $cbr_bw $tcp1_id $tcp2_id $sink1 $sink2"
 
     $ns run
 }
 
-proc run-experiment-2 {} {
-    foreach {combo} $VARIANT_COMBOS {
-        set tcp1 [lindex $combo 0]
-        set tcp2 [lindex $combo 1]
-        # generate random config
-
-        for {set i 0} {$i<iterations} {incr x} {
-            run-single-test $i $tcp1 $tcp2 #other config
-        }
-    }
-}
-
 proc finish {} {
-    global ns f nf
-    $ns flush-trace
-    close $f
-    close $nf
+    global file
 
-    puts "running nam..."
-    exec nam out.nam &
+    close $file
+    # Run Python Script
     exit 0
 }
 
-run-experiment-2
+# Call from command line with arguments
+# ns experiment2.tcl <tcp_combo_number> <cbr_bandwidth>
+proc main {argv argc} {
+    global VARIANT_COMBOS
+    set tcp1 [lindex [lindex $VARIANT_COMBOS [lindex $argv 0]] 0]
+    set tcp2 [lindex [lindex $VARIANT_COMBOS [lindex $argv 0]] 1]
+    run-single-test $tcp1 $tcp2 [lindex $argv 1]
+}
+
+main $argv $argc
